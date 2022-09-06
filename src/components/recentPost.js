cfa1cf6423db7e5ac18d596df5820720a7de8408
Sean Townz <saguailar13@gmail.com>
2022-09-06 04:15:02 -0700

    Create Post COmponent

--- src/components/recentPost.js
+++ src/components/recentPost.js
@@ -1,27 +1,42 @@
-import React, { Component } from "react";
-
-import { connect } from "react-redux"
-
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import * as actions from '../actions';
-class RecentPost extends Component {
+import Post from './post';
+class RecentPosts extends Component {
     componentDidMount() {
-    this.props.fetchRecentPosts();
-}
-    render () {
+        this.props.fetchRecentPosts();
+    }
+
+    renderPosts = function() {
+        const posts = this.props.recentPosts.map((post, index) => {
+            if(index < 3) {
+                return (
+                   <Post {...post} key={index}/>
+                )   
+            }
+        })
+        return posts
+    }
+
+    render() {
         return (
-           <div className="recent-post">
-            <div className="recent-post-wrapper">
-                <div className="recent-post__heading">
-                    <ul className="recent-post__post">
-                    <li>recent post 1</li>
-                    <li>recent post 2</li>
-                    <li>recent post 3</li>
+            <div className="recent-posts">
+                <div className="recent-posts__wrapper">
+                    <div className="recent-posts__heading">Recent Posts</div>
+                    <ul className="recent-posts__posts">
+                        {this.renderPosts()}
                     </ul>
                 </div>
+
             </div>
-           </div>
         )
     }
 }
 
-export default connect(null, actions)(RecentPost); 
+function mapStateToProps(state) {
+    return {
+        recentPosts: state.posts.recentPosts
+    }
+}
+
+export default connect(mapStateToProps, actions)(RecentPosts);
